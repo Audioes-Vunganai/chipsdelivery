883e4ef8a347f25adef9c26d3e060032fbe21cfd
Audioes Vunganai <vunganaiaudioes@gmail.com>
2026-05-16 17:39:13 +0200

    mobile-menu

--- src/index.js
+++ src/index.js
@@ -1,24 +1,5 @@
- const slider = document.getElementById("testimonialSlider");
-const slides = document.querySelectorAll("#testimonialSlider > div");
-
-  let index = 0;
-
-  function updateSlider() {
-    slider.style.transform = `translateX(-${index * 100}%)`;
-  }
-
-  document.getElementById("nextBtn").addEventListener("click", () => {
-    index = (index + 1) % slides.length;
-    updateSlider();
-  });
-
-  document.getElementById("prevBtn").addEventListener("click", () => {
-    index = (index - 1 + slides.length) % slides.length;
-    updateSlider();
-  });
-
-  // Auto Slide
-  setInterval(() => {
-    index = (index + 1) % slides.length;
-    updateSlider();
-  }, 5000);
+const mobileBtn = document.getElementById("mobile-btn");
+const mobileMenu = document.getElementById("mobile-menu");
+mobileBtn.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
+});
