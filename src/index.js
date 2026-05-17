c39c8211449308a93dcd1bb20a536e69d27f358f
Audioes Vunganai <vunganaiaudioes@gmail.com>
2026-05-17 10:49:04 +0200

    otherpages

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+// Smooth scrolling for anchor links
 document.querySelector("html").style.scrollBehavior = "smooth";
 
+// Mobile menu toggle
 const mobileBtn = document.getElementById("mobile-btn");
 const mobileMenu = document.getElementById("mobile-menu");
 const mobileIcon = document.getElementById("mukuru");
@@ -10,3 +12,16 @@ mobileBtn.addEventListener("click", () => {
   mobileIcon.classList.toggle("fa-x");
   mobileIcon.classList.toggle("text-primary");
 });
+
+// header animation
+const movingHeader = document.getElementById("moving-header");
+let position = window.innerWidth;
+function animate() {
+  position -= 2;
+  movingHeader.style.transform = `translateX(${position}px)`;
+  if (position < -movingHeader.offsetWidth) {
+    position = window.innerWidth;
+  }
+  requestAnimationFrame(animate);
+}
+animate();
